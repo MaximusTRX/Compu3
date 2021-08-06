854c20ce2b2a0f03514a62b150da188b9e7f6567
MaximusTRX <bonninmaximiliano@gmail.com>
2021-08-06 18:01:44 -0300

    Second commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,19 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    cuunter = 0;
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->pushButton_2->setText("0");
 }
 
